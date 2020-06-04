1ffe22f6e6f66787490d037959d950ce97d82143
coderanac <coder.anac@gmail.com>
2020-06-04 15:57:00 -0300

    :zap: improves and adds classes

--- public/pages/login/index.js
+++ public/pages/login/index.js
@@ -1,24 +1,22 @@
-export default () =>{
-    const container = document.createElement("div");
-    const template = `
-      <form class="login-form">
-        <div>
-          <label for="email-input"><b>E-mail</b></label>
-          <input type="email" id="email-input" placeholder="Enter E-mail" required>
-          <label for="password-input"><b>Password</b></label>
-          <input type="password" id="password-input" placeholder="Enter Password" required>
-          <input type="submit" id="submit-user-form" value="Login">
-        </div>
+export default () => {
+  const container = document.createElement('section');
+  const template = `
+      <form class="login-form form">
+        <section class="form__box">
+          <input id="email-input" class="form__field" type="email" placeholder="Enter E-mail" required>
+          <input id="password-input" class="form__field" type="password" placeholder="Enter Password" required>
+          <input id="submit-user-form" class="form__button" type="submit" value="Login">
+        </section>
       </form> 
-        <div class="Login-with-gmail">
-          <p>Login with Gmail:</p>
-        </div>
-        <div>
+        <section class="enter">
+          <p class="enter__logingmail">Login with Gmail:</p>
+          <button id="firebaseui-auth-container"></button>
+        </section>
+        <section class="enter__register">
           <p>Don't have an account?</p>
-          <a href="/#auth">Create Here</a>
-          <div id="firebaseui-auth-container"></div>
-        </div>
+          <a class="enter__register-link" href="/#auth">register here</a>
+        </section>
     `;
-    container.innerHTML = template;
-    return container;
+  container.innerHTML = template;
+  return container;
 };
