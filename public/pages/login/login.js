68665e1258172fad3fffcf40a5a713fd36169e74
coderanac <coder.anac@gmail.com>
2020-06-04 15:57:13 -0300

    :recycle: refac

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,20 +1,20 @@
-export function loginEmail(){
-  
-}
-  
-export function loginGmail(ui){
-    ui.reset();
-    ui.start('#firebaseui-auth-container', {
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-          return true;
-        },
+export function loginEmail() {
 
+}
+
+export function loginGmail(ui) {
+  ui.reset();
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+        return true;
       },
-      signInFlow: 'popup',
-      signInSuccessUrl: '#home'
-    });
-  }
+
+    },
+    signInFlow: 'popup',
+    signInSuccessUrl: '#home',
+  });
+}
