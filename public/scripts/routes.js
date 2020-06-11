6aa3980af0f1f233d6f182ae5cf6fe0330735771
coderanac <coder.anac@gmail.com>
2020-06-11 04:46:29 -0300

    :building_construction: change folder routes

---
+++ public/scripts/routes.js
@@ -0,0 +1,9 @@
+import login from '../pages/login/view.js'
+import register from '../pages/register/view.js';
+import home from '../pages/home/view.js';
+
+export default {
+  '#login': login(),
+  '#register': register(),
+  '#home': home(),
+};
