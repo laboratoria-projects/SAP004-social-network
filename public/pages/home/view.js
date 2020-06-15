ff691ddbd4e85a938cb44fbc7a3aef00689032ee
coderanac <coder.anac@gmail.com>
2020-06-15 19:50:02 -0300

    :poop: add events post icons

--- public/pages/home/view.js
+++ public/pages/home/view.js
@@ -2,30 +2,39 @@ import controllerHome from './controller.js'
 
 export default () => {
   const template = `
-    <nav class="nav-home">
-        <button class= "loggout-button" id="loggout">Loggout</button>
+    <header class="nav-home">
+        <i id="icon-menu" class="icon-menu menu"></i>
+        <nav id="nav" class="container-menu-burguer disable">
+          <ul class="list-menu">
+            <li>
+              Option 1
+            </li>
+            <li>
+              Option 2
+            </li>
+            <li class= "loggout-button" id="loggout">
+                Loggout
+            </li>
+          </ul>
+        </nav>
         <h1 class="title-knowledge">Knowledge</h1>
-        <div class="container-menu-burguer">
-            <input type="checkbox" id="checkbox-menu">
-            <label for="checkbox-menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
-        </div>
-    </nav>
-    <form id="form-post" class="post-form">
-        <input class="post-input" type="text" name="post" placeholder="Write your post!" />
-        <div class="buttons-post">
-          <div>
-            <input class="selector-checkbox" type="checkbox" id="private-selector" name="audience">
-            <label class="private-checkbox" for="private-selector">Private post</label>
-          </div>
-            <button class="submit-post-button" type="submit">Post</button>
-        </div>
-    </form>
+    </header>
+    <main class="mainly-home">
+      <form id="form-post" class="post-form">
+          <input class="post-input" type="text" name="post" placeholder="Write your post!" />
+          <section class="buttons-post">
+            <section>
+              <label id="lock" class="private-checkbox" for="private-selector">
+                <i id="icon-lock" class="icon-lock-open"></i>
+              </label>
+              <input class="selector-checkbox" type="checkbox" id="private-selector" name="audience">
+            </section>
+              <button class="submit-post-button" type="submit">Share</button>
+          </section>
+      </form>
 
-    <ul id="list-posts" class="post-feed"></ul>
+      <ul id="list-posts" class="post-feed"></ul>
+    </main>
   `;
 
   return controllerHome(template);
