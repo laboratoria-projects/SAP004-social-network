2e0268e714b0cedd1b20eed6c3fa0d580b64aff5
coderanac <coder.anac@gmail.com>
2020-06-15 10:10:34 -0300

    :art: add privte button

--- public/pages/home/view.js
+++ public/pages/home/view.js
@@ -1,11 +1,13 @@
 import controllerHome from './controller.js'
 
-export default () =>{
+export default () => {
   const template = `
     <button id="loggout">deslogar</button>
 
     <form id="form-post">
       <input type="text" name="post" placeholder="No que vc está pensando?"/>
+      <input type="checkbox" id="private-selector" name="audience">
+      <label for="private-selector">Private post</label>
       <button type="submit">Postar</button>
     </form>
 
