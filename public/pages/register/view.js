4ede8bd5aa7f4a35af8506c3ff118fa917511d77
coderanac <coder.anac@gmail.com>
2020-06-15 10:09:03 -0300

    :art: add semantic

--- public/pages/register/view.js
+++ public/pages/register/view.js
@@ -2,23 +2,30 @@ import controllerRegister from './controller.js'
 
 export default () => {
   const template = `
-    <section class="login_header">
-      <img src="../../img/logo.svg"/>
-    </section>
-    <form id="form-register" class="register-e-mail-form form">
-      <section class="form__box">
+    <header class="header">
+      <img class="logo-mobile" src="../../img/logo.svg"/>
+      <img class="logo-desktop" src="../../img/logo-desktop.svg"/>
+    </header>
+    <main class="container__auth">
+      <form id="form-register" class="form__box">
+        <fieldset class="form-title">
+            <h1>Knowlegde</h1>
+        </fieldset>
         <input class="form__field" type="email" name="email" autocomplete="email" placeholder="Enter E-mail" required />
         <input class="form__field" type="password" name="password" autocomplete="new-password" placeholder="Enter Password" required />
         <input class="form__button" type="submit" value="Create an account" />
+      </form> 
+      <section class="enter">
+        <p class="option_form">Create with</p>
+        <button id="gmail-auth" class="button-gmail">
+          <i class="icon-gmail"></i>
+        </button>
+        <p class="back">
+          Have a register? 
+          <a href="#login" class="option_form">Back to Login</a>
+        </p>
       </section>
-    </form> 
-    <section class="register-gmail-section enter">
-      <p class="option_form">Create with</p>
-      <button id="gmail-auth" class="button-gmail">
-        <i class="icon-gmail"></i>
-      </button>
-      <a href="#login" class="option_form">Back to Login</a>
-    </section>
+    </main>
   `;
 
   return controllerRegister(template);
