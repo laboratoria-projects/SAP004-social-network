100844d044700093a80a161e2eb599526ec6c794
Thais Durynek <thais.durynek@gmail.com>
2020-06-22 14:15:47 -0300

    :sparkles: personal feed funcionando

--- public/pages/personalfeed/controller.js
+++ public/pages/personalfeed/controller.js
@@ -1,60 +1,48 @@
-async function renderPersonalPosts() {
-  const posts = await firebase.firestore()
+import {
+  eventsPost
+} from '../home/controller.js'
+
+async function renderOnlyUserPosts() {
+  const postes = await firebase.firestore()
     .collection('postagens')
     .orderBy('date', 'desc')
     .get();
-
-  const personalPosts = document.querySelector('#personal-posts');
-
-  personalPosts.innerHTML = '';
-
-  const html = [];
-
-  // if (postRef.data().user !== firebase.auth().currentUser.uid) {
-  //   post.style.display = "none";
-  // } 
-  // else {
-    post.forEach(
-      (postRef) => {
+  postes.forEach(
+    (post) => {
+      if (post.data().user === firebase.auth().currentUser.uid) {
+        const ul = document.querySelector('#personal-posts');
         const li = document.createElement('li');
-        const post = postRef.data();
-      
         li.innerHTML = `
-                        <h3 class="user-name">${post.user}</h3>
-                        <p class="message-post">${post.text}</p>
-                        <section class="list-buttons">
-                          <button class="like-button">
-                            ${post.likes}
-                            <i class="icon-heart heart-clicked"></i>
-                          </button>
-                          <button class="edite-button">
-                            <i class="icon-pencil"></i>
-                          </button>
-                          <button class="delete-button">
-                            <i class="icon-bin"></i>
-                          </button>
-                          <button class="audience-button">
-                            <i id="icon-lock" class="${audience}"></i>
-                          </button>
-                        </section> 
-                    `;
-
-        li.id = postRef.id;
+                      <h3 class="user-name">${post.data().user}</h3>
+                      <p class="message-post">${post.data().text}</p>
+                      <section class="list-buttons">
+                        <button class="like-button">
+                          ${post.data().likes}
+                          <i class="icon-heart heart-clicked"></i>
+                        </button>
+                        <button class="edite-button">
+                          <i class="icon-pencil"></i>
+                        </button>
+                        <button class="delete-button">
+                          <i class="icon-bin"></i>
+                        </button>
+                      </section> 
+                      `;
+        li.id = post.id;
         li.classList.add('list');
-        li.post = post;
-
-        html.push(li);
-      },
-    );
-    personalPosts.append(...html);
-  // }
-};
+        li.post = post.data();
+        ul.appendChild(li);
+        eventsPost(ul);
+      }
+    });
+}
 
 function controllerPersonalPosts(template) {
   const container = document.createElement('div');
   container.classList.add('home');
   container.innerHTML = template;
-  renderPersonalPosts();
+  renderOnlyUserPosts();
   return container;
 }
+
 export default controllerPersonalPosts;
